c98c1efe5bede8be1ee4f1da6cce2778cece639c
hieutm211 <hieutm211@gmail.com>
2021-04-19 12:34:02 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen text-6xl font-semibold text-green-900 bg-green-50">
+      Hi there!
+    </div>
+  )
+}
+
+export default App
